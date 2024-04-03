90ca896e99cb9216183ba582857a66b691e82ed1
leomenvn <6003035@viewnext.com>
2024-04-03 14:04:40 +0200

    pantalla principal en XML

--- app/src/main/java/com/example/iberdrola/ui/MainActivity.kt
+++ app/src/main/java/com/example/iberdrola/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.iberdrola
+package com.example.iberdrola.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.iberdrola.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
