ec50236bcd87b950d48af417453d36eda18918ed
leomenvn <6003035@viewnext.com>
2024-04-05 14:45:22 +0200

    Creación del activity Login y su pantalla en XML

---
+++ app/src/main/java/com/example/iberdrola/ui/Login.kt
@@ -0,0 +1,24 @@
+package com.example.iberdrola.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.iberdrola.R
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun onTextViewClicked(view: View) {}
+}
