c4b9464e2de04afde024dd8143b29837575732b0
leomenvn <6003035@viewnext.com>
2024-04-08 10:33:36 +0200

    Creados activities y XMLs relacionados al auth

--- app/src/main/java/com/example/iberdrola/ui/auth/ResetPasswordActivity.kt
+++ app/src/main/java/com/example/iberdrola/ui/auth/ResetPasswordActivity.kt
@@ -1,24 +1,21 @@
-package com.example.iberdrola.ui
+package com.example.iberdrola.ui.auth
 
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.iberdrola.R
 
-class Login : AppCompatActivity() {
+class ResetPasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_reset_password)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
-
-    fun onTextViewClicked(view: View) {}
 }
